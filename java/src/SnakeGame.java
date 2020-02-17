849ad9c892d441d438ceb1d5f60a695ee183ca8f
Matthew <mdherrera3@miners.utep.edu>
2020-02-17 14:35:28 -0700

    All the constructors, elements, and methods

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -1,2 +1,21 @@
 public class SnakeGame {
+    private boolean[][] game;
+    private int[] headPosition;
+    private static int exhaustiveChecks;
+    private static int recursiveChecks;
+
+    public SnakeGame() {
+        game = new boolean[1][1];
+    }
+
+    public SnakeGame(boolean[][] game, int x, int y) {
+        this.game = game;
+    }
+    public int[] findTailExhaustively(){}
+    public int[] findTailRecursive(){}
+    private int[] findTailRecursively(int[] currentPosition, int[] previousPosition){}
+    private void resetCounter(){}
+    private static int getRecursiveChecks(){}
+    private static int getExhaustiveChecks(){}
+
 }
