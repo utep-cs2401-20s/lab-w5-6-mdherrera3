f790c226023a186651c1b9ab5b502b320e6eb938
Matthew <mdherrera3@miners.utep.edu>
2020-02-19 14:46:35 -0700

    properly implemented SnakeGame with three inputs, started on exhaustive search finished reset counter, and the getters for the checks

--- java/src/SnakeGame.java
+++ java/src/SnakeGame.java
@@ -8,14 +8,41 @@ public class SnakeGame {
         game = new boolean[1][1];
     }
 
-    public SnakeGame(boolean[][] game, int x, int y) {
-        this.game = game;
+    public SnakeGame(boolean[][] g, int x, int y) {
+        headPosition = new int[] {x,y};
+        game = g;
     }
-    public int[] findTailExhaustively(){}
-    public int[] findTailRecursive(){}
+    public int[] findTailExhaustively(){
+        int x = headPosition[0];
+        int y = headPosition[1];
+        int length = 0;
+        int[] result;
+        for(int i = 0; i < game.length; i++){
+            for(int j = 0; j < game[0].length; j++){
+                int counter = 0;
+                exhaustiveChecks++;
+                if(game[i][j]){
+                    length++;
+
+                }
+            }
+        }
+        result = new int [] {x,y, length};
+        return result;
+    }
+    public int[] findTailRecursive(){
+
+    }
     private int[] findTailRecursively(int[] currentPosition, int[] previousPosition){}
-    private void resetCounter(){}
-    private static int getRecursiveChecks(){}
-    private static int getExhaustiveChecks(){}
+    private void resetCounter(){
+        exhaustiveChecks = 0;
+        recursiveChecks = 0;
+    }
+    private static int getRecursiveChecks(){
+        return recursiveChecks;
+    }
+    private static int getExhaustiveChecks(){
+        return exhaustiveChecks;
+    }
 
 }
